99543bb3b5960090314ac15c2efa110096e2fd1a
Ashish Kumar <14398ashish@gmail.com>
2021-03-12 08:53:52 +0530

    Logistic Regression and Visualization

--- logisticRegression.R
+++ logisticRegression.R
@@ -1,4 +1,5 @@
 #Loading the dataset
+#Machine Learning From A-z kirill eremenko
 library("tidyverse")
 library(caTools)
 dataset=read_csv("Data/Social_Network_Ads.csv")
@@ -20,9 +21,47 @@ classifier = glm(formula=Purchased~.,
 
 prob_pred = predict(classifier,type='response',newdata=test_set[-3])
 y_pred=ifelse(prob_pred>0.5,1,0)
-print(y_pred)
-matrixcm = table(test_set$Purchased,y_pred)
+cm = table(test_set$Purchased,y_pred)
 
-class(test_set[,3])
-class(y_pred)
-classtest
+#Visualising the training set result
+#install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set= training_set
+x1 = seq(min(set[,1])-1,max(set[,1])+1,by = 0.01)
+x2 = seq(min(set[,2])-1, max(set[,2])+1,by=0.01)
+grid_set = expand.grid(x1,x2)
+colnames(grid_set) = c('Age','EstimatedSalary')
+prob_set = predict(classifier,type='response',
+                   newdata=grid_set)
+y_grid = ifelse(prob_set > 0.5,1,0)
+plot(set[,-3],main='Logistic Regression(Training Set)',
+     xlab = 'Age',
+     ylab = 'EstimatedSalary',
+     xlim = range(x1),
+     ylim= range(x2))
+contour(x1,x2,matrix(as.numeric(y_grid),
+                     length(x1),length(x2)),add=TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#Visualising the test set result
+library(ElemStatLearn)
+set= training_set
+x1 = seq(min(set[,1])-1,max(set[,1])+1,by = 0.01)
+x2 = seq(min(set[,2])-1, max(set[,2])+1,by=0.01)
+grid_set = expand.grid(x1,x2)
+colnames(grid_set) = c('Age','EstimatedSalary')
+prob_set = predict(classifier,type='response',
+                   newdata=grid_set)
+y_grid = ifelse(prob_set > 0.5,1,0)
+plot(set[,-3],main='Logistic Regression(Training Set)',
+     xlab = 'Age',
+     ylab = 'EstimatedSalary',
+     xlim = range(x1),
+     ylim= range(x2))
+contour(x1,x2,matrix(as.numeric(y_grid),
+                     length(x1),length(x2)),add=TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
