78199cb236bd0524feffd6430b75719c1f34b5c0
Ashish Kumar <14398ashish@gmail.com>
2021-03-12 09:25:31 +0530

    Adds Comment2

--- logisticRegression.R
+++ logisticRegression.R
@@ -4,7 +4,7 @@ library("tidyverse")
 library(caTools)
 dataset=read_csv("Data/Social_Network_Ads.csv")
 
-
+#Splitting the dataset
 set.seed(123)
 split=sample.split(dataset$Purchased,SplitRatio=3/4)
 training_set = subset(dataset,split==TRUE)
@@ -14,11 +14,12 @@ test_set=subset(dataset,split==FALSE)
 training_set[,1:2] =scale(training_set[,1:2])
 test_set[,1:2]=scale(test_set[,1:2])
 
-
+#Train the Logistic Regression on Training_set
 classifier = glm(formula=Purchased~.,
                  family=binomial,
                  data=training_set)
 
+#predict the result on test set
 prob_pred = predict(classifier,type='response',newdata=test_set[-3])
 y_pred=ifelse(prob_pred>0.5,1,0)
 cm = table(test_set$Purchased,y_pred)
